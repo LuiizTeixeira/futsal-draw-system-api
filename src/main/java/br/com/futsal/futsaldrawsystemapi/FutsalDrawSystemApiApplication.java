d56e1e19a604472d1a093a6cb1889bd75f80de7c
Danilo Azevedo <57817919+Daniloazevedo-dev@users.noreply.github.com>
2025-12-30 21:12:05 -0300

    refactor: corrige estrutura do projeto (#1) (#2)
    Co-authored-by: Luiz Teixeira <104911539+LuizTeixeira-1@users.noreply.github.com>

---
+++ src/main/java/br/com/futsal/futsaldrawsystemapi/FutsalDrawSystemApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.futsal.futsaldrawsystemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutsalDrawSystemApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FutsalDrawSystemApiApplication.class, args);
+    }
+}
