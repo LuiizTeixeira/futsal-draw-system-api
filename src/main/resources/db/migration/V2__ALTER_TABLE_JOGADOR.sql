8ade1a93657c606ee730e1adcb2bb6a984400319
Danilo Azevedo <danilo.ann@gmail.com>
2026-04-18 20:39:45 -0300

    feat: Altera e Adiciona versoes do migration

--- src/main/resources/db/migration/V2__ALTER_TABLE_JOGADOR.sql
+++ src/main/resources/db/migration/V2__ALTER_TABLE_JOGADOR.sql
@@ -1,4 +1,3 @@
-ALTER TABLE futsal.jogador
-    ADD COLUMN habilidade INT NOT NULL CHECK (habilidade >= 1 AND habilidade <= 5),
-    ADD COLUMN numero INT UNIQUE,
-    ADD COLUMN presenca CHAR(1) DEFAULT 'N' NOT NULL CHECK (presenca IN ('S', 'N'));
+ALTER TABLE futsal.jogador ADD COLUMN habilidade INT NOT NULL CHECK (habilidade >= 1 AND habilidade <= 5);
+ALTER TABLE futsal.jogador ADD COLUMN numero INT UNIQUE;
+ALTER TABLE futsal.jogador ADD COLUMN presenca CHAR(1) DEFAULT 'N' NOT NULL CHECK (presenca IN ('S', 'N'));
